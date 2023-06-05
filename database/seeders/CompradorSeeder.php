07eb5f3ce06ed9e240b21cb1270741a9fbe1eff9
Caca404 <christia10k@hotmail.com>
2023-06-05 11:53:35 -0300

    fix: remove comprador seed creation

--- database/seeders/CompradorSeeder.php
+++ database/seeders/CompradorSeeder.php
@@ -15,9 +15,9 @@ class CompradorSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Comprador::factory()->create([
-            'credits' => 100000,
-        ]);
+        // \App\Models\Comprador::factory()->create([
+        //     'credits' => 100000,
+        // ]);
         // \App\Models\Comprador::factory(5)
         //     ->has(Produto::factory()->count(2))
         //     ->create();
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AdminSeeder::class,
-            CompradorSeeder::class,
             VendedorSeeder::class
         ]);
     }
